a7335fb713dced4ae27811c0480193a9150beb57
Constance Amatey <consamate57@gmail.com>
2024-03-20 22:55:26 +0000

    addition

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -2,4 +2,4 @@
 "Write a type-annotated function add that takes a float a and a float b as arguments and returns their sum as a float"
 
 def add(a:float, b:float):
-    return(a+b)
+    return (a + b)
